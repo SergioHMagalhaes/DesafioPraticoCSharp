d2d34cb548e0790fa5725dc0fc06a255d25ed767
SergioHMagalhaes <ssergioh3@gmail.com>
2025-04-23 14:20:22 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioPraticoCSharp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DesafioPraticoCSharp;
+
+class Program {
+    static void Main()
+    {
+        Console.Write("Digite o seu nome: ");
+        string? name = Console.ReadLine();
+
+        Console.WriteLine($"Olá, {name}! Seja muito bem-vindo!");
+    }
+}
